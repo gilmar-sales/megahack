ecf37b1c18f438293fed122f72e9387cb9938a72
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-02-13 19:19:50 -0200

    feat(backend): add initial server settings

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 ### Padrões de escrita
 
 1. camelCase
-2. Aspas simples
+2. Aspas duplas
 3. Com ponto e vírgulas
-  
+4. Tab de 2 espaços
+
 ### Iniciando
 
 #### NodeJS
-  Acesse o site do [NodeJS](https://nodejs.org/en/download/) e baixe a versão 12.
+
+Acesse o site do [NodeJS](https://nodejs.org/en/download/) e baixe a versão 12.
 
 #### Instalando módulos do node
+
 `npm install -g expo-cli yarn`
---
+++ backend/src/config/database.js
@@ -0,0 +1,19 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+});
+
+module.exports = {
+  dialect: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  storage: "./__tests__/database.sqlite",
+  logging: false,
+  operetorAliases: false,
+  define: {
+    timestemps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
