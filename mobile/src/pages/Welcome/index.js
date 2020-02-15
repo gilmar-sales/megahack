15461ab6492bea435a6771dd7cb15dc7a838c4fd
gilmarxd <gilmar_custodio_@hotmail.com>
2020-02-15 16:40:08 -0300

    fix: import RoundedButton component

--- mobile/src/pages/Welcome/index.js
+++ mobile/src/pages/Welcome/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+import { RoundedButton } from "../../components/Buttons";
 
 // import { Container } from './styles';
 
