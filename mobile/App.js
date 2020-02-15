1b175bc099e454c88a4d23e0e763bdc6674cf037
gilmarxd <gilmar_custodio_@hotmail.com>
2020-02-15 16:21:58 -0300

    style: changed statusbar style to dark-content

--- mobile/App.js
+++ mobile/App.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import { View, StatusBar } from "react-native";
 import Routes from "./src/routes";
 
 export default function App() {
-  return <Routes />;
+  return (
+    <View style={{ width: '100%', height: '100%' }}>
+      <StatusBar barStyle="dark-content" />
+      <Routes />
+    </View>
+  );
 }
